a026c4216a1758f1903c58e04624921fb5b32fed
Scott Lorimor <scott.lorimor28@gmail.com>
2016-03-31 19:17:10 -0600

    implemented: mongodb hook

--- app.js
+++ app.js
@@ -3,14 +3,24 @@
 var express = require('express');
 var cors = require('cors');
 var bodyParser = require('body-parser');
+var mongo = require('mongojs');
 
-// initialize app & port
+// initialize app, server & DB
 var app = express();
 var port = 2020;
+var db = mongo('ecommTest', ['test']);
+
+// middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(cors);
+
 
 
 app.listen(port, function() {
 	console.log("listening on port: ", port);
-	console.log(app);
+	// console.log(app);
 });
+
+
 
