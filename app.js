c4e71b3e86e11a07d03ec19756ef7909f6186a9b
Scott Lorimor <scott.lorimor28@gmail.com>
2016-03-31 20:51:33 -0600

    implemented: post & get routes. mongodb connected

--- app.js
+++ app.js
@@ -8,12 +8,12 @@ var mongo = require('mongojs');
 // initialize app, server & DB
 var app = express();
 var port = 2020;
-var db = mongo('ecommTest', ['test']);
+var db = mongo('ecommerce', ['products']); // (dbName, ['collections to work with'])
 
 // middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
-app.use(cors);
+app.use(cors());
 
 
 
@@ -22,5 +22,54 @@ app.listen(port, function() {
 	// console.log(app);
 });
 
+app.get('/api/products', function(req,res) {
+	console.log("Remote address", req.connection.remoteAddress, 
+		"connected to the server with GET @: /api/products on", Date());
+	// res.json('endpoint @: /api/products connected.');
+	
+	var query = req.query;
+	db.products.find(query,function(err, resp) {
+		if (err) res.status(500).json(err);
+
+		res.json(resp);
+});
+
+app.get('/api/products/:id', function(req,res) {
+	console.log("Remote address", req.connection.remoteAddress, 
+		"connected to the server with GET @: /api/products/:id on", Date());
+	var temp = "You have selected a "+req.params.id;
+
+	});
+
+});
+
+app.post('/api/products', function(req, res) {
+	console.log("Remote address", req.connection.remoteAddress, 
+		"connected to the server with POST @: /api/products/ on", Date());
+	var temp = "You have added a "+req.body.type;
+
+	db.products.save(req.body, function(err,resp) {
+		if (err) res.status(500).json(err);
+
+		res.json(temp);
+	})
+
+})
+
+
+app.put('/api/products/:id', function(req,res) {
+	console.log("Remote address", req.connection.remoteAddress, 
+		"connected to the server with PUT @: /api/products/:id on", Date());
+	var temp = "You have edited a "+req.params.id;
+	res.json(temp);
+
+});
+
+app.delete('/api/products/:id', function(req,res) {
+	console.log("Remote address", req.connection.remoteAddress, 
+		"connected to the server with Delete @: /api/products/:id on", Date());
+	var temp = "You have DELETED a "+req.params.id;
+	res.json(temp);	
+});
 
 
